c18bb2ea6e5bfe58132529e0547c49c5334dfd63
tranhuynhkyanh <thkyanhlxag@gmail.com>
2024-04-16 20:24:47 +0700

    update

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from core import settings
 
 urlpatterns = [
-    path('api/v1/predict', include('apps.predict.urls')),
+    path('api/v1/predict/', include('apps.predict.urls')),
     path('admin/', admin.site.urls),
     ]
 
